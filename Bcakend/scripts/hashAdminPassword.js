0e3bd2334af710c61020d043d88b433ae5a5fab4
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2025-06-17 03:28:32 +0530

    push after login register

--- Bcakend/scripts/hashAdminPassword.js
+++
@@ -1,8 +0,0 @@
-// scripts/hashAdminPassword.js
-const bcrypt = require('bcrypt');
-
-(async () => {
-  const password = 'Admin@123';
-  const hashed = await bcrypt.hash(password, 10);
-  console.log('Hashed Password:', hashed);
-})();
--- Bcakend/src/config/passport.js
+++
@@ -1,5 +0,0 @@
-// src/config/passport.js
-require('dotenv').config(); // ✅ Load .env variables early
-const passport = require('passport');
-require('../strategies/google.strategy'); // now this will get env variables
-module.exports = passport;
